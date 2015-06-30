b150d9c792548b6afba7c50acbb315e1a37a0d42
Bill Mote <bill@android-fu.com>
2015-06-29 21:38:57 -0400

    Wire the views to the activity.

--- app/src/main/java/com/androidfu/intro2android/MainActivity.java
+++ app/src/main/java/com/androidfu/intro2android/MainActivity.java
@@ -4,13 +4,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView temperature;
+    private TextView location;
+    private Button update_temperature;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        temperature = (TextView) findViewById(R.id.tv_temperature);
+        location = (TextView) findViewById(R.id.tv_location);
+        update_temperature = (Button) findViewById(R.id.btn_update_temperature);
+        update_temperature.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // do something fun :)
+            }
+        });
+        
     }
 
     @Override
